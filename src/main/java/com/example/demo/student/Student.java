575ee7fbf1ec6e0d2f6aabfbc1a6a70f650b1d6a
IvanFernandez2000 <ivanagustin2012@gmail.com>
2025-07-17 21:03:27 -0300

    feat: create table student with JPA annotations

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,25 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_secuence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "Student_sequence"
+    )
+
     private Long id;
     private String name;
     private String emails;
